e76dd8d79b567683c55da7bfc678f2cfb0bc3076
huncsikmilan2011-byte <huncsikmilan2011@gmail.com>
2026-06-27 00:16:30 +0200

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sztoja.game',
+  appName: 'SztojaGame',
+  webDir: 'www'
+};
+
+export default config;
