6f1ea59be373dd56c3cf9cf91eb44aed078aaeab
huncsikmilan2011-byte <huncsikmilan2011@gmail.com>
2026-06-27 10:20:53 +0200

    Add AdMob plugin configuration to capacitor.config.ts
    Added AdMob plugin configuration with test App ID.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.sztoja.game',
   appName: 'SztojaGame',
-  webDir: 'www'
+  webDir: 'www',
+  // IDE jönnek a plugin beállítások:
+  plugins: {
+    AdMob: {
+      // Ez itt a hivatalos Google Android teszt App ID. 
+      // Ha élesíteni akarod, majd cseréld le a sajátodra!
+      appId: 'ca-app-pub-3940256099942544~3347511713'
+    }
+  }
 };
 
 export default config;
